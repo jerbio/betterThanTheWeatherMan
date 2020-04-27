12a57d329462179279d9c689013162d5a0bb7913
jeromebiotidara <jeromebiotidara@gmail.com>
2020-04-27 07:46:08 +0000

    File renames

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from implementations.onlypositivedeltas import runExec
 from libfiles.idealpricedsymbols import downloadedSymbols,subSetOfTech,SubsetOfFinance
 from libfiles.downloadstockdata import groupSymbolRequest
-import tensorflow as tf
-physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# import tensorflow as tf
+# physical_devices = tf.config.list_physical_devices('GPU') 
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 # groupSymbolRequest(downloadedSymbols);
 
-runExec(downloadedSymbols)
+# runExec(subSetOfTech)
+runExec(SubsetOfFinance)
+# runExec(["AAPL"])
