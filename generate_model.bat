177a186172dd8a18fa36f8fec986b607d1912257
jeromebiotidara <jeromebiotidara@gmail.com>
2020-06-03 07:35:14 -0600

    Cleaned up of forecasting checks. Removed the inefficient daily loop

--- generate_model.bat
+++ generate_model.bat
@@ -1,1 +1,7 @@
+cd "C:\Users\jerom\Documents\workspace\python\betterthantheweatherman\"
+
 "python.exe" "C:\Users\jerom\Documents\workspace\python\betterthantheweatherman\dailybots\builddailymodel.py"
+
+
+PAUSE
+SLEEP 10
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 # tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-# print("\n\n\nUsing subSetOfTech dataset"); runExec(subSetOfTech); print("\n\n\nUsing subSetOfTech dataset"+str(datetime.datetime.now()))
-print("\n\n\nUsing SubsetOfFinance dataset"); runExec(SubsetOfFinance[120:144]); print("\n\n\nUsing SubsetOfFinance dataset"+str(datetime.datetime.now()))
+print("\n\n\nUsing subSetOfTech dataset subSetOfTech[90:152]"); runExec(subSetOfTech[0:70]); print("\n\n\nUsing subSetOfTech dataset subSetOfTech[90:152]"+str(datetime.datetime.now()))
+# print("\n\n\nUsing SubsetOfFinance dataset SubsetOfFinance[0:72]"); runExec(SubsetOfFinance[0:72]); print("\n\n\nUsing SubsetOfFinance[0:72] dataset "+str(datetime.datetime.now()))
 # print("\n\n\nUsing downloadedSymbols dataset"); runExec(maxSymbols[:50]); print("\n\n\nUsing downloadedSymbols dataset"+str(datetime.datetime.now()))
 # # runExec(["AAPL"])
