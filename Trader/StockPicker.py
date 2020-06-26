0f4eddcd033a7872d3da86425ab987fcd8483549
jerbio <jeromebiotidara@gmail.com>
2020-06-26 17:39:07 -0600

    Initial pieces of robinhood trade framework

--- Trader/StockPicker.py
+++ Trader/StockPicker.py
@@ -5,12 +5,15 @@ PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
+from notpinky.implementations import weathermanpredictionconfig
+
 
 from pathlib import Path
 
 class StockPicker:
-    def __init__(self, isRemote = False):
+    def __init__(self,config:weathermanpredictionconfig, isRemote = False):
         self.isRemote = isRemote
+        self.config = config
     def retrieveLatestEligibleStocks():
         if self.isRemote:
             return self.getLatestStocksRemote()
@@ -19,7 +22,9 @@ class StockPicker:
 
 
     def getLatestStocksLocal():
-        
+        pathPrefix = '../notpinky/'
+        fullFolderPath = pathPrefix+self.config.predictionFolderTurnedKey
+        fullFolderPath = str(Path(fullFolderPath))
         
 
     def getLatestStocksRemote():
